cbf0646b42df101d5c8aba23a30cec2be1a01b9a
Christina Harris <christinathedesigner@outlook.com>
2018-12-27 13:40:55 -0800

    edited files

--- python_megacourse/to_file.py
+++ python_megacourse/to_file.py
@@ -1,9 +1,11 @@
 numbers = [1, 2, 3]
-file = open("to_file.txt", "w")
+file = open("resources/to_file.txt", "w")
 for i in numbers:
      file.write(str(i) + "\n")
 file.close()
 
 myfile = open("resources/to_file.txt")
 file = myfile.read()
-print(file)
+file = file.splitlines()
+for i in file:
+    print(i)
