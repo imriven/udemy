69b45f5ba6befd9355b6141f9a8f70092a186d69
Christina Harris <christinathedesigner@outlook.com>
2018-12-31 16:10:38 -0800

    while loop

---
+++ python_megacourse/while_loop.py
@@ -0,0 +1,19 @@
+x = 0
+while x < 3:
+    print("Smaller")
+    x = x+1
+
+
+
+"""
+for item in [1, 2, 3]:
+    print(i)
+
+for loop will run until the end of the list
+
+--------
+while condition:
+    do something
+
+a while loop will run until the condition is true
+"""
