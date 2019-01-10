d5cd7bb8388370f233386e6e63da29356ef7baa6
Christina Harris <christinathedesigner@outlook.com>
2019-01-10 14:47:00 -0800

    added on css and sass course. still building dictionary program.

--- python_megacourse/dictionary_app/app1.py
+++ python_megacourse/dictionary_app/app1.py
@@ -1,12 +1,27 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("resources/data.json"))
 
 def translate(w):
+    w = w.lower()
+    # in our data set the words are only lowercase -  we want to make this as user friendly program
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+      #  return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+        ask_word =  input("Did you mean %s instead? Please type Y or N: " % get_close_matches(w, data.keys())[0])
+        correct_word = get_close_matches(w, data.keys())[0]
+        if ask_word == "Y":
+            return  correct_word
+        elif ask_word == "N":
+            return "Your word wasn't found. Try again."
+        else:
+            return "Please"
+            
     else:
         return "The word doesn't exist. Please double check it."
+
 word = input("Enter word: ")
 
 print(translate(word))
--- python_megacourse/dictionary_app/look_up.py
+++ python_megacourse/dictionary_app/look_up.py
@@ -1,11 +1,32 @@
-# create a function that uses the input as the key and then returns the value for that key. try to incorporate loading of JSON file and print the output
 import json
+from difflib import get_close_matches
 
-def look_up_word(word, path):
-    word_dictionary = open(path, "r")
-    content_dict = json.load(word_dictionary)
-    if word in content_dict:
-        print(content_dict[word])
+data = json.load(open("resources/data.json"))
 
-look_up = input("What word would you like to look up?: ")
-look_up_word(look_up, "resources/data.json")
+def ask_match(word):
+
+    ask_word =  input("Did you mean %s instead? Please type Y or N: " % word)
+    if ask_word.lower() == "y":
+        return True
+    elif ask_word.lower() == "n":
+        return False
+    else:
+        return "Please try again"   
+
+def translate(w):
+    w = w.lower()
+    # in our data set the words are only lowercase -  we want to make this as user friendly program
+    if w in data:
+        return data[w]
+
+    closest_matches = get_close_matches(w, data.keys())
+    if len(closest_matches) > 0:
+        for match in closest_matches:
+            if ask_match(match):
+                return data[match]
+    else:
+        return "The word doesn't exist. Please double check it."
+
+word = input("Enter word: ")
+
+print(translate(word))
